be993c57fb1de06f48aa3c39e4967fb29f0b91ee
prathampithalia <prathampithalia100@gmail.com>
2023-10-06 14:46:30 +0530

    S

---
+++ Codeforces/B_ICPC_Balloons.cpp
@@ -0,0 +1,68 @@
+// #include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+
+#define all(a) a.begin(),a.end()
+#define fi first
+#define se second
+#define pb push_back
+#define pyes cout<<"YES"<<endl
+#define pno cout<<"NO"<<endl
+#define ce cout<<endl
+#define forr(i,n) for(int i=0 ; i<n ; i++)
+#define pvec(x)  for(auto &a : x){cout<<a<<" ";}
+#define pvec2d(v) for(int i=0 ; i<v.size() ; i++){for(int j=0 ; j<v[i].size() ; j++){cout<<v[i][j]<<" ";}cout<<endl;}
+
+#define each(a, x) for (auto &a : x)
+#define ll long long
+#define MOD 1000000007;
+#define test_cases(T) int T ; cin>>T ; while(T--)
+
+
+
+
+
+
+
+int main()
+{
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    test_cases(Y)
+    {
+        int n;
+        cin >> n;
+        string s;
+        cin >> s;
+        int vs = 26;
+        int v[vs]{0};
+
+        for (char i : s)
+        {
+            v[i - 'A']++;
+        }
+
+        int cnt = 0;
+        for (int i = 0; i < vs; i++)
+        {
+            if (v[i] == 1)
+                cnt += 2;
+            if (v[i] > 1)
+            {
+                cnt += 2;
+                v[i]--;
+                cnt += v[i];
+            }
+        }
+
+        cout << cnt << endl;
+    }
+}
