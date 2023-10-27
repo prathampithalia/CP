70b7960e840ccaff5a5765bf65f39ac301efc0f2
prathampithalia <prathampithalia100@gmail.com>
2023-10-27 15:02:21 +0530

    S

---
+++ Codeforces/A_Candies.cpp
@@ -0,0 +1,56 @@
+// #include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+
+#define all(a) a.begin(),a.end()
+#define fi first
+#define se second
+#define pb push_back
+#define pyes cout<<"YES"<<endl
+#define pno cout<<"NO"<<endl
+#define ce cout<<endl
+#define forr(i,n) for(int i=0 ; i<n ; i++)
+#define pvec(x)  for(auto &a : x){cout<<a<<" ";}
+#define pvec2d(v) for(int i=0 ; i<v.size() ; i++){for(int j=0 ; j<v[i].size() ; j++){cout<<v[i][j]<<" ";}cout<<endl;}
+
+#define each(a, x) for (auto &a : x)
+#define ll long long
+#define MOD 1000000007;
+#define test_cases(T) int T ; cin>>T ; while(T--)
+
+ll gcd(ll a, ll b) { if (b > a) { return gcd(b, a); } if (b == 0) { return a; } return gcd(b, a % b); }
+
+
+
+
+
+int main()
+{
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif 
+
+    ios_base::sync_with_stdio(0); cin.tie(0);
+
+    vector<ll int> p2{ 3,7,15,31,63,127,255,511,1023,2047,4095,8191,16383,32767,65535,131071,262143,524287,1048575,2097151,4194303,8388607,16777215,33554431,67108863,134217727,268435455,536870911,1073741823,2147483647 };
+
+    ll int m = p2.size();
+
+
+    test_cases(Y) {
+        ll int n;
+        cin >> n;
+
+        ll int ans = INT_MIN;
+
+        for (int i = 0; i < m; i++) {
+            if (n % p2[i] == 0) { ans = max(ans, n / p2[i]); }
+
+        }
+        cout << ans << endl;
+    }
+
+}
