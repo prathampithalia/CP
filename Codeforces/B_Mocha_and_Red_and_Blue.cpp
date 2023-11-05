de4be566e2b3b25951307494473ff05392aa7830
prathampithalia <prathampithalia100@gmail.com>
2023-11-06 01:03:28 +0530

    S

---
+++ Codeforces/B_Mocha_and_Red_and_Blue.cpp
@@ -0,0 +1,82 @@
+// #include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+
+#define all(a) a.begin(),a.end()
+#define fi first
+#define se second
+#define pb push_back
+#define pyes cout<<"YES"<<endl
+#define pno cout<<"NO"<<endl
+#define ce cout<<endl
+#define forr(i,n) for(int i=0 ; i<n ; i++)
+#define pvec(x)  for(auto &a : x){cout<<a<<" ";}
+#define pvec2d(v) for(int i=0 ; i<v.size() ; i++){for(int j=0 ; j<v[i].size() ; j++){cout<<v[i][j]<<" ";}cout<<endl;}
+
+#define each(a, x) for (auto &a : x)
+#define ll long long
+#define MOD 1000000007;
+#define test_cases(T) int T ; cin>>T ; while(T--)
+
+
+
+
+
+
+
+int main()
+{
+
+#ifndef ONLINE_JUDGE
+   freopen("input.txt", "r", stdin);
+   freopen("output.txt", "w", stdout);
+#endif 
+
+   ios_base::sync_with_stdio(0); cin.tie(0);
+
+
+   test_cases(Y) {
+      int n; cin >> n;
+      string s; cin >> s;
+      bool ok = false;
+      for (int i = 0; i < n;i++) { if (s[i] == 'B' || s[i] == 'R')ok = true; }
+
+      if (ok) {
+         int ind = 0;
+         while (ind < n)
+         {
+            for (int i = 0;i < n;i++) {
+               if (i != (n - 1) && s[i] == '?')
+               {
+                  if (s[i + 1] == 'B')s[i] = 'R';
+                  if (s[i + 1] == 'R')s[i] = 'B';
+               }
+            }
+            ind++;
+         }
+         ind = 0;
+         while (ind < n)
+         {
+            for (int i = 1;i < n;i++) {
+               if (s[i] == '?')
+               {
+                  if (s[i - 1] == 'B')s[i] = 'R';
+                  if (s[i - 1] == 'R')s[i] = 'B';
+               }
+            }
+            ind++;
+         }
+      }
+      else {
+         for (int i = 0; i < n;i++) {
+            if (i % 2)s[i] = 'R';
+            else s[i] = 'B';
+         }
+      }
+
+      cout << s << endl;
+   }
+
+
+}
