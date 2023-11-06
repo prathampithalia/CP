09d53419fe539c79822d3211cb01a2fed6a7e9cf
prathampithalia <prathampithalia100@gmail.com>
2023-11-06 23:10:56 +0530

    S

---
+++ Codeforces/A_Towers.cpp
@@ -0,0 +1,60 @@
+// #include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+
+#define all(a) a.begin(),a.end()
+#define fi first
+#define se second
+#define pb push_back
+#define pyes cout<<"YES"<<endl
+#define pno cout<<"NO"<<endl
+#define ce cout<<endl
+#define forr(i,n) for(int i=0 ; i<n ; i++)
+#define pvec(x)  for(auto &a : x){cout<<a<<" ";}
+#define pvec2d(v) for(int i=0 ; i<v.size() ; i++){for(int j=0 ; j<v[i].size() ; j++){cout<<v[i][j]<<" ";}cout<<endl;}
+
+#define each(a, x) for (auto &a : x)
+#define ll long long
+#define MOD 1000000007;
+#define test_cases(T) int T ; cin>>T ; while(T--)
+
+
+
+
+
+
+
+int main() 
+{
+
+#ifndef ONLINE_JUDGE
+   freopen("input.txt", "r", stdin);
+   freopen("output.txt", "w", stdout);
+#endif 
+
+   ios_base::sync_with_stdio(0); cin.tie(0);
+
+
+   int n; cin >> n;
+   vector<int> v(n);
+
+   forr(i, n)cin >> v[i];
+
+   vector<int> t(1001);
+
+   for (int i : v)
+   {
+      t[i]++;
+   }
+   int a1 = INT_MIN, a2 = 0;
+
+   for (int i = 0; i < 1002; i++) {
+      a1 = max(a1, t[i]);
+      if (t[i] != 0) a2++;
+   }
+
+   cout << a1 << " " << a2 << endl;
+
+
+}
