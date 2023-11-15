b0ca18bd8e3f219e07200858a9dd9f58da32876e
prathampithalia <prathampithalia100@gmail.com>
2023-11-16 03:12:48 +0530

    S

---
+++ Codeforces/B_Chemistry.cpp
@@ -0,0 +1,59 @@
+// #include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+
+#define all(a) a.begin(),a.end()
+#define fi first
+#define se second
+#define pb push_back
+#define pyes cout<<"YES"<<endl
+#define pno cout<<"NO"<<endl
+#define ce cout<<endl
+#define forr(i,n) for(int i=0 ; i<n ; i++)
+#define pvec(x)  for(auto &a : x){cout<<a<<" ";}
+#define pvec2d(v) for(int i=0 ; i<v.size() ; i++){for(int j=0 ; j<v[i].size() ; j++){cout<<v[i][j]<<" ";}cout<<endl;}
+
+#define each(a, x) for (auto &a : x)
+#define ll long long
+#define MOD 1000000007;
+#define test_cases(T) int T ; cin>>T ; while(T--)
+
+
+
+
+
+
+
+int main() 
+{
+
+#ifndef ONLINE_JUDGE
+freopen("input.txt", "r", stdin);
+freopen("output.txt", "w", stdout);
+#endif 
+
+ios_base::sync_with_stdio(0); cin.tie(0);
+
+
+test_cases(Y) {
+    int n, k; cin >> n >> k;
+    string s; cin >> s;
+
+    vector<int> t(26);
+    for (char i : s) {
+        t[i - 'a']++;
+    }
+
+    int x = 0;
+    for (int i : t) {
+        if (i % 2 == 1)x++;
+    }
+    if (x > k + 1) pno;
+    else pyes;
+
+
+}
+
+
+}
