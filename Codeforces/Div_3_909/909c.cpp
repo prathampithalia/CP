21f2cd6825900076716abc0e5a6faff8d91d1ef8
prathampithalia <prathampithalia100@gmail.com>
2023-11-17 22:28:37 +0530

    WA , a : correct

---
+++ Codeforces/Div_3_909/909c.cpp
@@ -0,0 +1,67 @@
+// #include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+
+#define all(a) a.begin(),a.end()
+#define fi first
+#define se second
+#define pb push_back
+#define pyes cout<<"YES"<<endl
+#define pno cout<<"NO"<<endl
+#define ce cout<<endl
+#define forr(i,n) for(int i=0 ; i<n ; i++)
+#define pvec(x)  for(auto &a : x){cout<<a<<" ";}
+#define pvec2d(v) for(int i=0 ; i<v.size() ; i++){for(int j=0 ; j<v[i].size() ; j++){cout<<v[i][j]<<" ";}cout<<endl;}
+
+#define each(a, x) for (auto &a : x)
+#define ll long long
+#define MOD 1000000007;
+#define test_cases(T) int T ; cin>>T ; while(T--)
+
+
+
+
+
+
+
+int main() 
+{
+
+#ifndef ONLINE_JUDGE
+freopen("input.txt", "r", stdin);
+freopen("output.txt", "w", stdout);
+#endif 
+
+ios_base::sync_with_stdio(0); cin.tie(0);
+
+test_cases(Y){
+    int n;
+    cin>>n;
+    vector<ll int> v(n);
+    forr(i,n)cin>>v[i];
+
+    vector<int> p(n);
+
+    long long maxi = LONG_MIN; 
+    long long sum = 0;
+
+    for (int i = 0; i < n; i++) {
+
+        if( (v[i]%2 != v[i+1]%2) && (v[i]%2 != v[i-1]%2)  ){
+            
+        sum += v[i] ;
+        }
+        else {sum=v[i];}
+
+        maxi= max(maxi,sum);
+    }
+
+    cout<<maxi<<endl;
+
+
+}
+
+
+
+}
