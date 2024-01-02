8ce3ff9d7151909bd8ac69f5fa066f766e6f110e
prathampithalia <prathampithalia100@gmail.com>
2024-01-02 23:33:13 +0530

    S

---
+++ Codeforces/A_Is_It_a_Cat.cpp
@@ -0,0 +1,65 @@
+// #include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+
+#define all(a) a.begin(),a.end()
+#define fi first
+#define se second
+#define pb push_back
+#define pyes cout<<"YES"<<endl
+#define pno cout<<"NO"<<endl
+#define ce cout<<endl
+#define forr(i,n) for(int i=0 ; i<n ; i++)
+#define pvec(x)  for(auto &a : x){cout<<a<<" ";}
+#define pvec2d(v) for(int i=0 ; i<v.size() ; i++){for(int j=0 ; j<v[i].size() ; j++){cout<<v[i][j]<<" ";}cout<<endl;}
+
+#define each(a, x) for (auto &a : x)
+#define ll long long
+#define MOD 1000000007 
+#define test_cases(T) int T ; cin>>T ; while(T--)
+
+
+bool compf(pair<int, int> a, pair<int, int> b) {
+
+    if( a.first == b.first )return a.second <b.second ;
+    else return a.first > b.first;
+
+}
+
+
+
+int main() 
+{
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif 
+
+    ios_base::sync_with_stdio(0); cin.tie(0);
+
+
+    test_cases(Y) {
+        int n; cin >> n;
+        string s;cin >> s;
+
+        string f = "";
+
+        f.pb(tolower(s[0]));
+
+        char prev = f[0];
+
+        for (int i = 0;i < n;i++) {
+            if (tolower(s[i]) != f.back()) {
+                f.pb(tolower(s[i]));
+            }
+        }
+
+        // cout<< f <<endl;
+        if (f == "meow") { pyes; }
+        else pno;
+    }
+
+
+}
