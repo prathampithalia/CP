8dc2a4771ec96b5d24a03804b7034cf4666142a1
prathampithalia <prathampithalia100@gmail.com>
2024-02-02 00:09:21 +0530

    S

--- Codeforces/A_Insomnia_cure.cpp
+++ Codeforces/A_Insomnia_cure.cpp
@@ -4,26 +4,31 @@ using namespace std;
 
 
 #define all(a) a.begin(),a.end()
-
-#define forr(i,n) for(int i=0 ; i< n ;i++) 
-#define pvec(x)  for(auto &a : x){cout<<a<<" ";}
-#define pvec2d(v) for(int i=0 ; i<v.size() ; i++){for(int j=0 ; j<v[i].size() ; j++){cout<<v[i][j]<<" ";}cout<<endl;}
-#define seev(v,n)  for(int i=0;i<n;i++){ll int ___; cin>>___; v.push_back(___);}
-
-
-#define each(a, x) for (auto &a : x)
-#define vi vector<int>
-#define pb push_back
-#define ll long long
 #define fi first
 #define se second
-#define lb lower_bound
-#define ub upper_bound
-#define sumall(a) accumulate(a.begin(),a.end(),0)
-#define MOD 1000000007;
+#define pb push_back
+#define pyes cout<<"YES"<<endl
+#define pno cout<<"NO"<<endl
+#define ce cout<<endl
+#define forr(i,n) for(int i=0 ; i<n ; i++)
+#define pvec(x)  for(auto &a : x){cout<<a<<" ";}
+#define pvec2d(v) for(int i=0 ; i<v.size() ; i++){for(int j=0 ; j<v[i].size() ; j++){cout<<v[i][j]<<" ";}cout<<endl;}
+
+#define each(a, x) for (auto &a : x)
+#define ppair(x) each(i,x)cout<<i.first<<" "<<i.second<<endl;
+
+
+#define ll long long
+#define MOD 1000000007 
 #define test_cases(T) int T ; cin>>T ; while(T--)
 
 
+bool compf(pair<int, int> a, pair<int, int> b) {
+
+    if( a.first == b.first )return a.second <b.second ;
+    else return a.first > b.first;
+
+}
 
 
 
@@ -31,18 +36,20 @@ int main()
 {
 
 #ifndef ONLINE_JUDGE
-freopen("input.txt", "r", stdin);
-freopen("output.txt", "w", stdout);
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
 #endif 
 
-ios_base::sync_with_stdio(0); cin.tie(0);
+    ios_base::sync_with_stdio(0); cin.tie(0);
 
-int k,l,m,n,d;
-cin>>k>>l>>m>>n>>d;
+    int s = 5;
 
-
-cout<<k+(2*l)+m+n;
-
-
+    vector<int> v(s);
+    forr(i, s)cin >> v[i];
+    int cnt = 0;
+    for (int i = 1;i <= v[4]; i++) {
+        if (i % v[0] == 0 || i % v[1] == 0 || i % v[2] == 0 || i % v[3] == 0)cnt++;
+    }
+    cout << cnt;
 
 }
