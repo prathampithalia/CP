5a22edca28495d267da259b7c1211448e4cab6b9
prathampithalia <prathampithalia100@gmail.com>
2024-02-01 11:40:43 +0530

    S

---
+++ CodeChef/119/A.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    // your code goes here
+    int n; cin >> n;
+
+    if (n >= 16)cout << "ADVITIYA";
+    else cout << "WAITING FOR ADVITIYA";
+
+}
