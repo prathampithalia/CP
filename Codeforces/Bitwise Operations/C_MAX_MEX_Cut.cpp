35f37a6f45a492c7977c16559ded4751992b3b68
prathampithalia <prathampithalia100@gmail.com>
2024-02-01 22:21:29 +0530

    WA

---
+++ Codeforces/Bitwise Operations/C_MAX_MEX_Cut.cpp
@@ -0,0 +1,83 @@
+// #include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+
+#define all(a) a.begin(),a.end()
+#define fi first
+#define se second
+#define pb push_back
+#define pyes cout<<"YES"<<endl
+#define pno cout<<"NO"<<endl
+#define ce cout<<endl
+#define forr(i,n) for(int i=0 ; i<n ; i++)
+#define pvec(x)  for(auto &a : x){cout<<a<<" ";}
+#define pvec2d(v) for(int i=0 ; i<v.size() ; i++){for(int j=0 ; j<v[i].size() ; j++){cout<<v[i][j]<<" ";}cout<<endl;}
+
+#define each(a, x) for (auto &a : x)
+#define ppair(x) each(i,x)cout<<i.first<<" "<<i.second<<endl;
+
+
+#define ll long long
+#define MOD 1000000007 
+#define test_cases(T) int T ; cin>>T ; while(T--)
+
+
+bool compf(pair<int, int> a, pair<int, int> b) {
+
+    if( a.first == b.first )return a.second <b.second ;
+    else return a.first > b.first;
+
+}
+
+#define int ll
+
+
+signed main() 
+{
+
+#ifndef ONLINE_JUDGE
+freopen("input.txt", "r", stdin);
+freopen("output.txt", "w", stdout);
+#endif 
+
+ios_base::sync_with_stdio(0); cin.tie(0);
+
+test_cases(Y){
+    int n; cin>>n;
+    string a , b;  cin>>a>>b;
+
+    vector<int> v(n);
+    
+    int t = 0;
+
+    forr(i,n){
+        if((a[i] + b[i]) == 97) v[i] = 2;
+        else if((a[i] + b[i]) == 96) v[i] = 1;
+        else{
+            v[i] = 0;
+        }
+        t+= v[i];
+    }
+
+    // pvec(v)
+    // ce;
+    int zn = 0 ;
+    
+    for(int i=0 ; i<n -1  ; i++){
+        if( v[i]== 0 && v[i+1] == 1){
+            zn++ ;
+            i+=2 ;
+        }
+        else if(v[i]== 1 && v[i+1] == 0){
+            zn++;
+            i+=2;
+        }
+    }   
+
+    cout << t + zn << endl;
+}   
+
+
+
+}
