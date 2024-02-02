c2a1bedab878dcaf8e20e46e5b260ddb8a4638b3
prathampithalia <prathampithalia100@gmail.com>
2024-02-02 22:38:26 +0530

    S

---
+++ Codeforces/A_Second_Order_Statistics.cpp
@@ -0,0 +1,72 @@
+// #include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+
+#define all(a) a.begin(),a.end()
+#define fi first
+#define se second
+#define pb push_back
+#define pyes cout<<"YES"<<endl
+#define pno cout<<"NO"<<endl
+#define ce cout<<endl
+#define forr(i,n) for(int i=0 ; i<n ; i++)
+#define pvec(x)  for(auto &a : x){cout<<a<<" ";}
+#define pvec2d(v) for(int i=0 ; i<v.size() ; i++){for(int j=0 ; j<v[i].size() ; j++){cout<<v[i][j]<<" ";}cout<<endl;}
+
+#define each(a, x) for (auto &a : x)
+#define ppair(x) each(i,x)cout<<i.first<<" "<<i.second<<endl;
+
+
+#define ll long long
+#define MOD 1000000007 
+#define test_cases(T) int T ; cin>>T ; while(T--)
+
+
+bool compf(pair<int, int> a, pair<int, int> b) {
+
+    if( a.first == b.first )return a.second <b.second ;
+    else return a.first > b.first;
+
+}
+
+bool sorted(vector<int> &v , int l , int r){
+    for(int i= l ; i<r ; i++){
+        if( v[i] > v[i+1]) return false;
+    }
+    return true;
+}
+
+int main() 
+{
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif 
+
+    ios_base::sync_with_stdio(0); cin.tie(0);
+
+
+    int n; cin >> n;
+    set<int> v;
+    forr(i, n) {
+        int x; cin >> x;
+        v.insert(x);
+    }
+    int ans = 0;
+    int j = 0;
+    each(i, v) {
+        if (j == 1) {
+            ans = i;
+            break;
+        }
+
+        j++;
+    }
+
+    if (v.size() > 1)
+        cout << ans << endl;
+    else pno;
+
+}
