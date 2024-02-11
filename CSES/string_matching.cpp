59fa4b2027220425e3cb5bbb61873af437c6c22b
prathampithalia <prathampithalia100@gmail.com>
2024-02-11 13:10:02 +0530

    Solved : prefix func & Z algo

--- CSES/string_matching.cpp
+++ CSES/string_matching.cpp
@@ -1,57 +1,77 @@
-#include <iostream>
+// #include <iostream>
+#include<bits/stdc++.h>
 using namespace std;
- 
- 
+
+
 #define all(a) a.begin(),a.end()
- 
-#define pvec(a,x)  for(auto &a : x){cout<<a<<" ";}
-#define pvec2d(v) for(int i=0 ; i<v.size() ; i++){for(int j=0 ; j<v[i].size() ; j++){cout<<v[i][j]<<" ";}cout<<endl;}
-#define seev(v,n)  for(int i=0;i<n;i++){ll int ___; cin>>___; v.push_back(___);}
- 
- 
-#define each(a, x) for (auto &a : x)
-#define vi vector<int>
+#define fi first
+#define se second
 #define pb push_back
+#define pyes cout<<"YES"<<endl
+#define pno cout<<"NO"<<endl
+#define ce cout<<endl
+#define forr(i,n) for(int i=0 ; i<n ; i++)
+#define pvec(x)  for(auto &a : x){cout<<a<<" ";}
+#define pvec2d(v) for(int i=0 ; i<v.size() ; i++){for(int j=0 ; j<v[i].size() ; j++){cout<<v[i][j]<<" ";}cout<<endl;}
+
+#define each(a, x) for (auto &a : x)
+#define ppair(x) each(i,x)cout<<i.first<<" "<<i.second<<endl;
+
+
 #define ll long long
-#define lb lower_bound
-#define ub upper_bound
-#define sumall(a) accumulate(a.begin(),a.end(),0)
-#define MOD 1000000007;
+#define MOD 1000000007 
 #define test_cases(T) int T ; cin>>T ; while(T--)
- 
- 
- 
- 
+
+
+bool compf(pair<int, int> a, pair<int, int> b) {
+
+    if( a.first == b.first )return a.second <b.second ;
+    else return a.first > b.first;
+
+}
+
+vector<int> prefix_function(string s) {
+    int n = (int)s.length();
+    vector<int> pi(n);
+    for (int i = 1; i < n; i++) {
+        int j = pi[i - 1];
+        while (j > 0 && s[i] != s[j]) j = pi[j - 1];
+        if (s[i] == s[j]) j++;
+        pi[i] = j;
+    }
+    return pi;
+}
+
+
+vector<int> find_occurrences(string text, string pattern) {
+    string cur = pattern + '#' + text;
+    int sz1 = text.size(), sz2 = pattern.size();
+    vector<int> v;
+    vector<int> lps = prefix_function(cur);
+    for (int i = sz2 + 1; i <= sz1 + sz2; i++) {
+        if (lps[i] == sz2) v.push_back(i - 2 * sz2);
+    }
+    return v;
+}
+
+
+
 int main() 
 {
- 
+
 #ifndef ONLINE_JUDGE
 freopen("input.txt", "r", stdin);
 freopen("output.txt", "w", stdout);
 #endif 
- 
+
 ios_base::sync_with_stdio(0); cin.tie(0);
- 
- 
-string s,t; cin>>s>>t;
- 
-ll int j=0,i=0,cnt=0 ,ss=s.size() ,tt=t.size();
- 
-while(i<ss){
-    if(s[i]==t[j]){
-    ++i;
-    ++j;
- 
-    }
-    else {
-        // j=0;
-        i++;
- 
-    }
-    if( j == tt){cnt++; j=0;}
-}
- 
-cout<<cnt;
- 
- 
+
+
+string a, b ;cin>>a>>b;
+
+vector<int> v = find_occurrences(a,b);
+
+cout<< v.size();
+
+
 }
