76194438850ac476248dde022eb5379aefe7b649
prathampithalia <prathampithalia100@gmail.com>
2024-02-18 01:15:30 +0530

    S

--- Codeforces/B_Months_and_Years.cpp
+++ Codeforces/B_Months_and_Years.cpp
@@ -15,11 +15,12 @@ using namespace std;
 #define pvec2d(v) for(int i=0 ; i<v.size() ; i++){for(int j=0 ; j<v[i].size() ; j++){cout<<v[i][j]<<" ";}cout<<endl;}
 
 #define each(a, x) for (auto &a : x)
+#define ppair(x) each(i,x)cout<<i.first<<" "<<i.second<<endl;
+
+
 #define ll long long
-#define MOD 1000000007;
+#define MOD 1000000007 
 #define test_cases(T) int T ; cin>>T ; while(T--)
-
-
 
 
 
@@ -36,36 +37,29 @@ freopen("output.txt", "w", stdout);
 ios_base::sync_with_stdio(0); cin.tie(0);
 
 
+vector<int> months{ 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31,
+                31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31,
+                31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31,
+                31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31,
+                31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
 int n; cin>>n;
-vector<int> v(n);
+vector<int> a(n);
+forr(i,n)cin>>a[i];
+for(int i=0; i+n<=60; ++i) {
+		bool ok=true;
+		for(int j=0; j<n; ++j) {
+			if(months[i+j]!=a[j]) {
+				ok=false;
+				break;
+			}
+		}
+		if(ok) {
+			cout << "Yes";
+			return 0;
+		}
+	}
+	cout << "No";
 
-bool lp_check = false;
-forr(i,n){cin>>v[i]; if( v[i]==29) v[i]--;}
 
-vector<int> year{31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31, 
-				31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31, 
-				31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31, 
-				31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31, 
-				31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
-
-bool ok = false ;
-int j=0;
-int mx=INT_MIN ;
-for(int i=0 ; i < year.size(); i++){
-    if( year[i] == v[j] ){
-        // cout<<year[i%12]<<" " ;
-        // if( mx == n) break;
-        j++;
-        mx = max(mx , j);
-    }
-    else {
-        // if( i>= 12) i++;
-        // i++;
-        j=0 ;
-    }
-}
-
-// cout<<endl<<j<<endl;
-if(mx >= n) cout<<"Yes";
-else cout<<"No";
 }
