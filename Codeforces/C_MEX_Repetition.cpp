a4890a7b0aa6c787034ea5e7a5c7eead855051fb
prathampithalia <prathampithalia100@gmail.com>
2024-02-19 12:11:59 +0530

    S : editorial

--- Codeforces/C_MEX_Repetition.cpp
+++ Codeforces/C_MEX_Repetition.cpp
@@ -15,11 +15,12 @@ using namespace std;
 #define pvec2d(v) for(int i=0 ; i<v.size() ; i++){for(int j=0 ; j<v[i].size() ; j++){cout<<v[i][j]<<" ";}cout<<endl;}
 
 #define each(a, x) for (auto &a : x)
+#define ppair(x) each(i,x)cout<<i.first<<" "<<i.second<<endl;
+
+
 #define ll long long
-#define MOD 1000000007;
+#define MOD 1000000007 
 #define test_cases(T) int T ; cin>>T ; while(T--)
-
-
 
 
 
@@ -36,64 +37,28 @@ freopen("output.txt", "w", stdout);
 ios_base::sync_with_stdio(0); cin.tie(0);
 
 
-test_cases(Y){
-    int n,k;
-    cin>>n>>k;
+test_cases(Y) {
+    int n, k; cin >> n >> k;
+    vector<int> v(n);
 
-    vector<ll int> v(n);
-    ll int missing =  0 ;
-    ll int t[n+1]{0};
+    int mis = 0, sm = 0;
 
-    forr(i,n){
-        cin>>v[i];
-        t[v[i]]=1;
+    forr(i, n) {
+        cin >> v[i];
+        sm += v[i];
     }
 
-    forr(i,n){if( t[i]==0 ) missing = i ;}
-    vector<ll int > a(n);
+    mis = (n * (n + 1)) / 2 - sm;
 
-// 1
-// 2 0 1
-// 2 1
-// 2 3 4 5 0
-
-// 1 1
-// 0
-
-// 7 5 3 0 4 2 1 6 9 10
-
-if (n==1){
-    if( v[0]==0){
-        if( k%2==0) cout<< 0<<endl;
-        else cout<< 1<<endl;
-    }
-    else {
-        if( k%2==0) cout<< 1<<endl;
-        else cout<< 0<<endl;
-    }
-}    
-else{
-    if( (k/n)%2 == 0 ){
-        a[0]=missing;
-        for(int i=1 ;i<n ;i++){
-            a[i]=v[i-1];
-        }
-    }
-    else{
-        a[n-1]=missing;
-        for(int i=0 ;i<n-1 ;i++){
-            a[i]=v[i+1];
-        }        
-    }
-    pvec(a)
-    ce;
+    // editorial 
+    k %= (n + 1);
+    v.pb(mis);
+    rotate(v.begin(), v.end() - k, v.end());
+    v.pop_back();
+    pvec(v)
+        ce;
 }
-
-
 
 
 }
 
-
-
-}
