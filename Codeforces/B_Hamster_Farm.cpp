3d0a2af6cd56b250ed8c0583f2d007df4f1ea87d
prathampithalia <prathampithalia100@gmail.com>
2024-02-22 13:34:45 +0530

    S

---
+++ Codeforces/B_Hamster_Farm.cpp
@@ -0,0 +1,61 @@
+// #include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+
+#define all(a) a.begin(),a.end()
+#define fi first
+#define se second
+#define pb push_back
+#define pyes cout<<"YES"<<endl
+#define pno cout<<"NO"<<endl
+#define ce cout<<endl
+#define forr(i,n) for(int i=0 ; i<n ; i++)
+#define pvec(x)  for(auto &a : x){cout<<a<<" ";}
+#define pvec2d(v) for(int i=0 ; i<v.size() ; i++){for(int j=0 ; j<v[i].size() ; j++){cout<<v[i][j]<<" ";}cout<<endl;}
+
+#define each(a, x) for (auto &a : x)
+#define ppair(x) each(i,x)cout<<i.first<<" "<<i.second<<endl;
+
+
+#define ll long long
+#define MOD 1000000007 
+#define test_cases(T) int T ; cin>>T ; while(T--)
+
+#define large 1e18+1
+
+#define int ll
+
+signed main() 
+{
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif 
+
+    ios_base::sync_with_stdio(0); cin.tie(0);
+
+    int n, k; cin >> n >> k;
+
+    vector<int> v(k);
+    forr(i, k) {
+        cin >> v[i];
+
+    }
+
+    int mini = large, ind = 0;
+
+    forr(i, k) {
+
+        mini = min(mini, n % v[i]);
+        if (mini == (n % v[i]))
+            ind = i;
+
+    }
+
+    cout << ind + 1 << ' ' << n / v[ind];
+
+
+
+}
