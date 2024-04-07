e2b02a5eb4e6e3eaf59da6465d4ade11db55944b
prathampithalia <prathampithalia100@gmail.com>
2024-04-08 01:32:22 +0530

    S : prime , sieve

---
+++ LeetCode/204. Count Primes.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int countPrimes(int n) {
+        vector<bool> primes(n, 1);
+        int ans = 0;
+        for (int i = 2; i < n; ++i) {
+            if (primes[i]) {
+                ++ans;
+                for (int j = i; j < n; j += i)
+                    primes[j] = false;
+            }
+        }
+        return ans;
+    }
+};
