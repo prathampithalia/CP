61ca725d642232e79cb097e9ab3302aa9dcc896e
prathampithalia <prathampithalia100@gmail.com>
2024-04-08 01:10:38 +0530

    S

---
+++ LeetCode/1492. The kth Factor of n.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int kthFactor(int n, int k) {
+
+        vector<int> f;
+        for (int i = 1;i <= n; i++) {
+            if (n % i == 0) f.push_back(i);
+        }
+
+        if (f.size() >= k) return f[k - 1];
+
+        return -1;
+
+
+    }
+};
