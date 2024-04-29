d6328033fda87c875e93f520d5317676d48489ab
Pratham Pithalia <144723251+prathampithalia@users.noreply.github.com>
2024-04-29 12:21:49 +0530

    s : using map

--- LeetCode/1365. How Many Numbers Are Smaller Than the Current Number.cpp
+++ LeetCode/1365. How Many Numbers Are Smaller Than the Current Number.cpp
@@ -16,3 +16,28 @@ public:
         return ans;
     }
 };
+
+class Solution {
+public:
+    vector<int> smallerNumbersThanCurrent(vector<int>& nums) {
+
+        int n = nums.size();
+
+        unordered_map<int, int> mp;
+        for (auto i : nums) {
+            mp[i]++;
+        }
+
+        vector<int> ans(n);
+        for (int i = 0; i < n; i++) {
+            int cnt = 0;
+            for (auto j : mp) {
+                if (nums[i] > j.first)
+                    cnt += j.second;
+            }
+            ans[i] = cnt;
+        }
+
+        return ans;
+    }
+};
