e3635a31be2233a7fa0c798d63da785a3913c3b2
Pratham Pithalia <144723251+prathampithalia@users.noreply.github.com>
2024-05-07 23:24:53 +0530

    S

---
+++ Codeforces/B_Array_Stabilization.cpp
@@ -0,0 +1,154 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define all(a) a.begin(), a.end()
+#define fi first
+#define se second
+#define pb push_back
+#define pyes cout << "YES" << endl
+#define pno cout << "NO" << endl
+#define ce cout << endl
+#define forr(i, n) for (int i = 0; i < n; i++)
+#define pvec(x) for (auto & a: x) { cout << a << " ";}
+#define pvec2d(v) for (int i = 0; i < v.size(); i++) {for (int j = 0; j < v[i].size(); j++) {cout << v[i][j] << " ";}cout << endl;}
+
+#define each(a, x) for (auto & a: x)
+#define ppair(x) each(i, x) cout << i.first << " " << i.second << endl;
+
+#define ll long long
+#define MOD 1000000007
+#define test_cases(T) int T; cin >> T; while (T--)
+
+//Check
+bool isPrime(ll n) {
+    if (n <= 1) return false;
+    if (n <= 3) return true;
+    if (n % 2 == 0 || n % 3 == 0) return false;
+    for (int i = 5; i * i <= n; i = i + 6)
+        if (n % i == 0 || n % (i + 2) == 0) return false;
+    return true;
+}
+bool isPowerOfTwo(int x) {
+    return x && !(x & (x - 1));
+}
+bool isPerfectSquare(ll x) {
+    if (x >= 0) {
+        ll sr = sqrt(x);
+        return (sr * sr == x);
+    }
+    return false;
+}
+
+//Constants
+vector < ll > primes;
+vector < bool > is_prime;
+
+// Mathematical functions
+void Sieve(int n) {
+    is_prime.assign(n + 1, true);
+    is_prime[0] = is_prime[1] = false;
+    for (ll i = 2; i * i <= n; i++)
+        if (is_prime[i])
+            for (ll j = i * i; j <= n; j += i) is_prime[j] = false;
+}
+void get_primes(int n) {
+    for (int i = 2; i <= n; i++)
+        if (is_prime[i]) primes.push_back(i);
+}
+ll mod_add(ll a, ll b, ll m) {
+    a = a % m;
+    b = b % m;
+    return (((a + b) % m) + m) % m;
+}
+ll mod_sub(ll a, ll b, ll m) {
+    a = a % m;
+    b = b % m;
+    return (((a - b) % m) + m) % m;
+}
+ll gcd(ll a, ll b) {
+    if (b == 0) return a;
+    return gcd(b, a % b);
+} //__gcd 
+ll lcm(ll a, ll b) {
+    return (a / gcd(a, b) * b);
+}
+ll moduloMultiplication(ll a, ll b, ll mod) {
+    ll res = 0;
+    a %= mod;
+    while (b) {
+        if (b & 1) res = (res + a) % mod;
+        b >>= 1;
+    }
+    return res;
+}
+ll powermod(ll x, ll y, ll p) {
+    ll res = 1;
+    x = x % p;
+    if (x == 0) return 0;
+    while (y > 0) {
+        if (y & 1) res = (res * x) % p;
+        y = y >> 1;
+        x = (x * x) % p;
+    }
+    return res;
+}
+
+ll modinv(ll p, ll q) {
+    ll ex;
+    ex = MOD - 2;
+    while (ex) {
+        if (ex & 1) {
+            p = (p * q) % MOD;
+        }
+        q = (q * q) % MOD;
+        ex >>= 1;
+    }
+    return p;
+}
+
+
+
+#ifndef ONLINE_JUDGE
+#include "template.cpp"
+#else
+#define debug(...)
+#define debugArr(...):
+#endif
+
+
+
+
+
+signed main() {
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+
+int n ;cin>>n;
+vector<int> v(n);
+forr(i,n)cin>>v[i];
+
+sort(all(v));
+
+// v.pop_back();
+
+// int a = *min_element(all(v));
+// int b = *max_element(all(v));
+
+// cout << b - a ;
+
+// ABOVE METHOD IS WRONG BECAUSE IT WILL FAIL ON : 1 4 6
+
+// IF 6 IS REMOVED THEN ANS - 3 , BUT THE ANS IS 2 
+
+// so we just remove either 1st or last 
+cout << min(v[n-1] - v[1] , v[n-2]- v[0]);
+
+}
