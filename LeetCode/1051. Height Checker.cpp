cd28e112f7d7e489753dc7733b21cd213321d414
Pratham Pithalia <144723251+prathampithalia@users.noreply.github.com>
2024-05-10 11:58:50 +0530

    S

---
+++ LeetCode/1051. Height Checker.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int heightChecker(vector<int>& heights) {
+        vector<int> v(heights.begin(),heights.end());
+
+        sort(v.begin(),v.end());
+        int ans = 0 ;
+        for(int i = 0 ;i<v.size() ; i++){
+            if(v[i] != heights[i])ans++;
+        }
+        return ans ;
+    }
+};
