fc7ee17b9c3978fa3559136d3838e1b7564bb488
Pratham Pithalia <144723251+prathampithalia@users.noreply.github.com>
2024-05-12 16:19:59 +0530

    S

--- Codeforces/A_String_Task.cpp
+++ Codeforces/A_String_Task.cpp
@@ -20,7 +20,7 @@ signed main() {
 
     for (int i = 0; i < s.size();i++) {
         s[i] = tolower(s[i]);
-        if (s[i] != 'a' && s[i] != 'e' && s[i] != 'i' && s[i] != 'o' && s[i] != 'u') {
+        if (s[i] != 'a' && s[i] != 'e' && s[i] != 'i' && s[i] != 'o' && s[i] != 'u' && s[i] != 'y') {
             ans += '.';
             ans += s[i];
         }
