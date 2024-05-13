52813f1b9318cf48b3aa6898baa086b6ad5db50e
Pratham Pithalia <144723251+prathampithalia@users.noreply.github.com>
2024-05-13 17:02:27 +0530

    S

---
+++ Codeforces/F_3_SUM.cpp
@@ -0,0 +1,90 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define all(a) a.begin(), a.end()
+#define fi first
+#define se second
+#define pb push_back
+#define pyes cout << "YES" << endl
+#define pno cout << "NO" << endl
+#define ce cout << endl
+#define forr(i, n) for (int i = 0; i < n; i++)
+#define pvec(x) for (auto & a: x) { cout << a << " ";}
+#define pvec2d(v) for (int i = 0; i < v.size(); i++) {for (int j = 0; j < v[i].size(); j++) {cout << v[i][j] << " ";}cout << endl;}
+
+#define each(a, x) for (auto & a: x)
+#define ppair(x) each(i, x) cout << i.first << " " << i.second << endl;
+
+#define ll long long
+#define MOD 1000000007
+#define test_cases(T) int T; cin >> T; while (T--)
+
+#define int ll
+
+
+
+signed main() {
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    test_cases(Y) {
+        int n; cin >> n;
+
+        vector<int> v(10);
+        forr(i, n) {
+            int x; cin >> x;
+            v[x % 10]++;
+        }
+
+        // 3 , 13 , 23 , 33 
+
+        // 33 and later not possible 
+
+
+        vector<int> vv = v;
+
+        bool ans = false;
+
+        // pvec(v) ;ce;
+        // sum == 3
+        if ((v[0] > 0 && v[1] >= 1 && v[2] >= 1) || (v[1] >= 3)) ans = true;
+        else {
+
+            for (int i = 0; i < 10; i++) {
+                if (v[i] > 0) {
+                    v[i]--;
+                    for (int j = 0; j < 10; j++) {
+                        if (v[j] > 0) {
+                            v[j]--;
+                            for (int k = 0; k < 10; k++) {
+                                if (v[k] > 0) {
+                                    // debug(i,j,k) ; ce;
+                                    if ((i + j + k) % 10 == 3)ans = true;
+                                }
+                            }
+                            if (ans == false) v[j]++;
+                        }
+                    }
+                    if (ans == false)v[i]++;
+                }
+            }
+
+
+        }
+
+        if (ans) pyes;
+        else pno;
+
+
+    }
+
+
+
+}
