4d7fb0e6ce742f53d23a3e3930822690fe59252b
Pratham Pithalia <144723251+prathampithalia@users.noreply.github.com>
2024-05-15 18:25:12 +0530

    S

---
+++ CSES/Finding_Borders.cpp
@@ -0,0 +1,96 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define all(a) a.begin(), a.end()
+#define fi first
+#define se second
+#define pb push_back
+#define pyes cout << "YES" << endl
+#define pno cout << "NO" << endl
+#define ce cout << endl
+#define forr(i, n) for (int i = 0; i < n; i++)
+#define pvec(x) for (auto & a: x) { cout << a << " ";}
+#define pvec2d(v) for (int i = 0; i < v.size(); i++) {for (int j = 0; j < v[i].size(); j++) {cout << v[i][j] << " ";}cout << endl;}
+
+#define each(a, x) for (auto & a: x)
+#define ppair(x) each(i, x) cout << i.first << " " << i.second << endl;
+
+#define ll long long
+#define MOD 1000000007
+#define test_cases(T) int T; cin >> T; while (T--)
+
+vector<int> z_function(string s) {
+  int n = (int)s.length();
+  vector<int> z(n);
+  for (int i = 1, l = 0, r = 0; i < n; ++i) {
+	if (i <= r && z[i - l] < r - i + 1) {
+	  z[i] = z[i - l];
+	} else {
+	  z[i] = max(0, r - i + 1);
+	  while (i + z[i] < n && s[z[i]] == s[i + z[i]]) ++z[i];
+	}
+	if (i + z[i] - 1 > r) l = i, r = i + z[i] - 1;
+  }
+  return z;
+}
+
+vector<int> prefix_function(string s) {
+	int n = (int)s.length();
+	vector<int> pi(n);
+	for (int i = 1; i < n; i++) {
+		int j = pi[i - 1];
+		while (j > 0 && s[i] != s[j]) j = pi[j - 1];
+		if (s[i] == s[j]) j++;
+		pi[i] = j;
+	}
+	return pi;
+}
+
+
+vector<int> find_occurrences(string text, string pattern) {
+	string cur = pattern + '#' + text;
+	int sz1 = text.size(), sz2 = pattern.size();
+	vector<int> v;
+	vector<int> lps = prefix_function(cur);
+	for (int i = sz2 + 1; i <= sz1 + sz2; i++) {
+		if (lps[i] == sz2) v.push_back(i - 2 * sz2);
+	}
+	return v;
+}
+
+
+
+signed main() {
+
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+
+	string s;cin >> s;
+
+	string t = s;
+
+
+	vector<int> a = prefix_function(s);
+
+	reverse(all(t));
+	vector<int> ta = z_function(t);
+
+	set<int> ans;
+	vector<int> an;
+
+	int j = a[s.size() - 1];
+	while (j > 0) {
+		ans.insert(j);
+		// an.pb(j);
+		j = a[j - 1];
+	}
+
+	pvec(a)
+
+}
