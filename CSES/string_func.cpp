8199c9c69a18cf96ecc6804f729968302b80f119
Pratham Pithalia <144723251+prathampithalia@users.noreply.github.com>
2024-05-15 17:50:06 +0530

    s

--- CSES/string_func.cpp
+++ CSES/string_func.cpp
@@ -1,59 +1,81 @@
-#include <bits/stdc++.h>
-using namespace std;
- 
- 
-#define all(a) a.begin(),a.end()
-#define forr(i,n) for(int i=0 ; i< n ;i++) 
-#define pvec(x)  for(auto &a : x){cout<<a<<" ";}
-#define pvec2d(v) for(int i=0 ; i<v.size() ; i++){for(int j=0 ; j<v[i].size() ; j++){cout<<v[i][j]<<" ";}cout<<endl;}
-#define seev(v,n)  for(int i=0;i<n;i++){ll int ___; cin>>___; v.push_back(___);}
- 
- 
-#define each(a, x) for (auto &a : x)
-#define vi vector<int>
-#define pb push_back
-#define ll long long
-#define lb lower_bound
-#define ub upper_bound
-#define sumall(a) accumulate(a.begin(),a.end(),0)
-#define MOD 1000000007;
-#define test_cases(T) int T ; cin>>T ; while(T--)
- 
+#include<bits/stdc++.h>
 
-int main() 
-{
+using namespace std;
+
+#define all(a) a.begin(), a.end()
+#define fi first
+#define se second
+#define pb push_back
+#define pyes cout << "YES" << endl
+#define pno cout << "NO" << endl
+#define ce cout << endl
+#define forr(i, n) for (int i = 0; i < n; i++)
+#define pvec(x) for (auto & a: x) { cout << a << " ";}
+#define pvec2d(v) for (int i = 0; i < v.size(); i++) {for (int j = 0; j < v[i].size(); j++) {cout << v[i][j] << " ";}cout << endl;}
+
+#define each(a, x) for (auto & a: x)
+#define ppair(x) each(i, x) cout << i.first << " " << i.second << endl;
+
+#define ll long long
+#define MOD 1000000007
+#define test_cases(T) int T; cin >> T; while (T--)
+
+vector<int> z_function(string s) {
+  int n = (int)s.length();
+  vector<int> z(n);
+  for (int i = 1, l = 0, r = 0; i < n; ++i) {
+	if (i <= r && z[i - l] < r - i + 1) {
+	  z[i] = z[i - l];
+	} else {
+	  z[i] = max(0, r - i + 1);
+	  while (i + z[i] < n && s[z[i]] == s[i + z[i]]) ++z[i];
+	}
+	if (i + z[i] - 1 > r) l = i, r = i + z[i] - 1;
+  }
+  return z;
+}
+
+vector<int> prefix_function(string s) {
+	int n = (int)s.length();
+	vector<int> pi(n);
+	for (int i = 1; i < n; i++) {
+		int j = pi[i - 1];
+		while (j > 0 && s[i] != s[j]) j = pi[j - 1];
+		if (s[i] == s[j]) j++;
+		pi[i] = j;
+	}
+	return pi;
+}
+
+
+vector<int> find_occurrences(string text, string pattern) {
+	string cur = pattern + '#' + text;
+	int sz1 = text.size(), sz2 = pattern.size();
+	vector<int> v;
+	vector<int> lps = prefix_function(cur);
+	for (int i = sz2 + 1; i <= sz1 + sz2; i++) {
+		if (lps[i] == sz2) v.push_back(i - 2 * sz2);
+	}
+	return v;
+}
+
+signed main() {
 
 #ifndef ONLINE_JUDGE
-freopen("input.txt", "r", stdin);
-freopen("output.txt", "w", stdout);
-#endif 
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
 
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
 
-string s;
-cin>>s;
+string s ; cin>>s;
 
+vector<int> a = z_function(s);
+vector<int> b = prefix_function(s);
 
-int n = s.size();
+pvec(a)
+ce;
+pvec(b)
 
-vector<int> z(n);
-int x = 0, y = 0;
-for (int i = 1; i < n; i++) {
-z[i] = max(0,min(z[i-x],y-i+1));
-while (i+z[i] < n && s[z[i]] == s[i+z[i]]) {
-x = i; y = i+z[i]; z[i]++;
 }
-}
-
-vector<int> pi(n);
-for (int i = 0; i < n; i++)
-    for (int k = 0; k <= i; k++)
-        if (s.substr(0, k) == s.substr(i-k+1, k))
-            pi[i] = k;
-
-
-pvec(z)
-cout<<endl;
-pvec(pi)
-}
-
-
