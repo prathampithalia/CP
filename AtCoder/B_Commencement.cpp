c8665db0b04e5006f905c551a581eaf1a4ad1dfe
Pratham Pithalia <144723251+prathampithalia@users.noreply.github.com>
2024-05-17 04:16:12 +0530

    S

---
+++ AtCoder/B_Commencement.cpp
@@ -0,0 +1,55 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define all(a) a.begin(), a.end()
+#define fi first
+#define se second
+#define pb push_back
+#define pyes cout << "YES" << endl
+#define pno cout << "NO" << endl
+#define ce cout << endl
+#define forr(i, n) for (int i = 0; i < n; i++)
+#define pvec(x) for (auto & a: x) { cout << a << " ";}
+#define pvec2d(v) for (int i = 0; i < v.size(); i++) {for (int j = 0; j < v[i].size(); j++) {cout << v[i][j] << " ";}cout << endl;}
+
+#define each(a, x) for (auto & a: x)
+#define ppair(x) each(i, x) cout << i.first << " " << i.second << endl;
+
+#define ll long long
+#define MOD 1000000007
+#define test_cases(T) int T; cin >> T; while (T--)
+
+
+signed main() {
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+string s ; cin>>s ;
+    
+vector<int> f(26);
+
+forr(i,s.size()){
+    f[s[i]-'a']++;
+}
+vector<int> ff(101);
+
+forr(i,26){
+    ff[f[i]]++;
+}
+bool ans = true ;
+for(int i = 1 ;i<101; i++){
+    if( ff[i] != 2 && ff[i] != 0) ans= false;
+}
+
+if( ans) cout << "Yes";
+else cout<<"No";
+
+
+}
