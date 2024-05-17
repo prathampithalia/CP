9d526b37c050d1700226ed9e9ba2848f7916a7e4
Pratham Pithalia <144723251+prathampithalia@users.noreply.github.com>
2024-05-17 22:40:52 +0530

    S

---
+++ AtCoder/C_Σ.cpp
@@ -0,0 +1,49 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define all(a) a.begin(), a.end()
+#define fi first
+#define se second
+#define pb push_back
+#define pyes cout << "YES" << endl
+#define pno cout << "NO" << endl
+#define ce cout << endl
+#define forr(i, n) for (int i = 0; i < n; i++)
+#define pvec(x) for (auto & a: x) { cout << a << " ";}
+#define pvec2d(v) for (int i = 0; i < v.size(); i++) {for (int j = 0; j < v[i].size(); j++) {cout << v[i][j] << " ";}cout << endl;}
+
+#define each(a, x) for (auto & a: x)
+#define ppair(x) each(i, x) cout << i.first << " " << i.second << endl;
+
+#define ll long long
+#define int ll
+#define MOD 1000000007
+#define test_cases(T) int T; cin >> T; while (T--)
+
+
+signed main() {
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+int n , k ; cin>>n>>k;
+
+int ans = (k*(k+1))/2 ;
+
+unordered_map<int,int>v;
+forr(i,n){
+    int x ; cin>>x;
+    v[x]++;
+    if( x <= k && v[x] == 1) ans -= x ;
+}
+
+cout<< ans ; 
+
+
+}
