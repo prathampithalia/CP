079f9e9360c02357093f06fa46e5c8b3bb9de728
Pratham Pithalia <144723251+prathampithalia@users.noreply.github.com>
2024-05-18 00:59:40 +0530

    S

---
+++ AtCoder/C_343.cpp
@@ -0,0 +1,57 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define all(a) a.begin(), a.end()
+#define fi first
+#define se second
+#define pb push_back
+#define pyes cout << "YES" << endl
+#define pno cout << "NO" << endl
+#define ce cout << endl
+#define forr(i, n) for (int i = 0; i < n; i++)
+#define pvec(x) for (auto & a: x) { cout << a << " ";}
+#define pvec2d(v) for (int i = 0; i < v.size(); i++) {for (int j = 0; j < v[i].size(); j++) {cout << v[i][j] << " ";}cout << endl;}
+
+#define each(a, x) for (auto & a: x)
+#define ppair(x) each(i, x) cout << i.first << " " << i.second << endl;
+
+#define ll long long
+#define MOD 1000000007
+#define int ll 
+#define test_cases(T) int T; cin >> T; while (T--)
+
+bool pal(string s){
+	forr(i ,s.size()/2){
+		if( s[i] != s[s.size() - 1-i]) return false;
+	}
+
+	return true ;
+}
+
+signed main() {
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+
+    int n;  cin >> n;
+
+
+    int ans = 1;
+    for (int i = 1; (i * i * i) <= n;i++) {
+        int k = (i * i * i);
+        string s = to_string(k);
+        if (pal(s)) {
+            ans = max(ans, k);
+        }
+    }
+
+    cout << ans;
+
+}
