aa11656678ae432fb3865ab810cc68fe7f69b72d
Pratham Pithalia <144723251+prathampithalia@users.noreply.github.com>
2024-05-21 17:56:45 +0530

    S

---
+++ AtCoder/B_Do_you_know_the_second_highest_mountain.cpp
@@ -0,0 +1,47 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define all(a) a.begin(), a.end()
+#define fi first
+#define se second
+#define pb push_back
+#define pyes cout << "YES" << endl
+#define pno cout << "NO" << endl
+#define ce cout << endl
+#define forr(i, n) for (int i = 0; i < n; i++)
+#define pvec(x) for (auto & a: x) { cout << a << " ";}
+#define pvec2d(v) for (int i = 0; i < v.size(); i++) {for (int j = 0; j < v[i].size(); j++) {cout << v[i][j] << " ";}cout << endl;}
+
+#define each(a, x) for (auto & a: x)
+#define ppair(x) each(i, x) cout << i.first << " " << i.second << endl;
+
+#define ll long long
+#define MOD 1000000007
+#define test_cases(T) int T; cin >> T; while (T--)
+
+
+signed main() {
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n; cin >> n;
+    vector<pair<int, string>> v;
+
+    forr(i, n) {
+        string s; int x;
+        cin >> s >> x;
+        v.pb({ x , s });
+    }
+
+    sort(all(v));
+
+    cout << v[n - 2].second;
+
+}
