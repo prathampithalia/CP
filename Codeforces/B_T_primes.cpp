6176a625f8e18fd0689dffb1aef545761fb5d211
Pratham Pithalia <144723251+prathampithalia@users.noreply.github.com>
2024-06-07 14:28:52 +0530

    S : square of prime are T primes

---
+++ Codeforces/B_T_primes.cpp
@@ -0,0 +1,103 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define all(a) a.begin(), a.end()
+#define fi first
+#define se second
+#define pb push_back
+#define pyes cout << "YES" << endl
+#define pno cout << "NO" << endl
+#define ce cout << endl
+#define forr(i, n) for (int i = 0; i < n; i++)
+#define each(a, x) for (auto & a: x)
+#define pvec(x) for (auto & a: x) { cout << a << " ";}
+#define pvec2d(v) for (int i = 0; i < v.size(); i++) {for (int j = 0; j < v[i].size(); j++) {cout << v[i][j] << " ";}cout << endl;}
+#define ppair(x) each(i, x) cout << i.first << " " << i.second << endl;
+
+#define ll long long
+#define MOD 1000000007
+#define test_cases(T) int T; cin >> T; while (T--)
+#define vec(v,n,int) vector<int> v(n) ; forr(i,n)cin>>v[i];
+#define vec2d(v,n,m,int) vector<vector<int>> v(n,vector<int> (m,0)); forr(i, n)forr(j, m) cin >> v[i][j];
+
+
+//Check
+bool isPrime(ll n) {
+    if (n <= 1) return false;
+    if (n <= 3) return true;
+    if (n % 2 == 0 || n % 3 == 0) return false;
+    for (int i = 5; i * i <= n; i = i + 6)
+        if (n % i == 0 || n % (i + 2) == 0) return false;
+    return true;
+}
+bool isPowerOfTwo(int x) {
+    return x && !(x & (x - 1));
+}
+bool isPerfectSquare(ll x) {
+    if (x >= 0) {
+        ll sr = sqrt(x);
+        return (sr * sr == x);
+    }
+    return false;
+}
+
+//Constants
+vector < ll > primes;
+vector < bool > is_prime;
+
+// Mathematical functions
+void Sieve(int n) {
+    is_prime.assign(n + 1, true);
+    is_prime[0] = is_prime[1] = false;
+    for (ll i = 2; i * i <= n; i++)
+        if (is_prime[i])
+            for (ll j = i * i; j <= n; j += i) is_prime[j] = false;
+}
+void get_primes(int n) {
+    for (int i = 2; i <= n; i++)
+        if (is_prime[i]) primes.push_back(i);
+}
+ll mod_add(ll a, ll b, ll m) {
+    a = a % m;
+    b = b % m;
+    return (((a + b) % m) + m) % m;
+}
+ll mod_sub(ll a, ll b, ll m) {
+    a = a % m;
+    b = b % m;
+    return (((a - b) % m) + m) % m;
+}
+ll gcd(ll a, ll b) {
+    if (b == 0) return a;
+    return gcd(b, a % b);
+} //__gcd 
+ll lcm(ll a, ll b) {
+    return (a / gcd(a, b) * b);
+}
+
+#define int ll
+
+
+
+signed main() {
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    Sieve(1e6);
+
+    int n; cin >> n;
+    forr(i, n) {
+        int x; cin >> x;
+        if (isPerfectSquare(x) && isPrime(sqrt(x))) pyes;
+        else pno;
+    }
+
+
+}
