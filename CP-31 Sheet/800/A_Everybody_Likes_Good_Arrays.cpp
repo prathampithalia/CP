632ce1f2d798f0fdd010f32afcfe245a2f1b64d6
Pratham Pithalia <144723251+prathampithalia@users.noreply.github.com>
2024-06-09 16:51:40 +0530

    S

--- CP-31 Sheet/800/A_Everybody_Likes_Good_Arrays.cpp
+++ CP-31 Sheet/800/A_Everybody_Likes_Good_Arrays.cpp
@@ -64,3 +64,85 @@ signed main() {
 
 
 }
+
+
+// Solution  2
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define all(a) a.begin(), a.end()
+#define fi first
+#define se second
+#define pb push_back
+#define pyes cout << "YES" << endl
+#define pno cout << "NO" << endl
+#define ce cout << endl
+#define forr(i, n) for (int i = 0; i < n; i++)
+#define each(a, x) for (auto & a: x)
+#define pvec(x) for (auto & a: x) { cout << a << " ";}
+#define pvec2d(v) for (int i = 0; i < v.size(); i++) {for (int j = 0; j < v[i].size(); j++) {cout << v[i][j] << " ";}cout << endl;}
+#define ppair(x) each(i, x) cout << i.first << " " << i.second << endl;
+
+#define ll long long
+#define MOD 1000000007
+#define test_cases(T) int T; cin >> T; while (T--)
+#define vec(v,n,int) vector<int> v(n) ; forr(i,n)cin>>v[i];
+#define vec2d(v,n,m,int) vector<vector<int>> v(n,vector<int> (m,0)); forr(i, n)forr(j, m) cin >> v[i][j];
+
+#define int ll
+
+bool isgood(vector<int> &v){
+
+	forr(i,v.size()-1){
+		if(v[i] == v[i+1])return false;
+	}
+	return true;
+}
+
+
+signed main() {
+
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+
+	test_cases(Y) {
+		int n; cin >> n;
+		vector<int> v(n);
+		set<int> s;
+		forr(i, n)
+		{
+			int x; cin >> x;
+			v[i] = (bool)(x & 1);
+			s.insert(v[i]);
+		}
+
+		if (isgood(v) || n == 1) cout << 0 << endl;
+		else {
+
+			int cnt = 0,cnt2= 0, A = 0;
+			for(int i =0 ;i<n ;i++){
+				if( v[i] ){
+					cnt++;
+					if (cnt2 > 0)A += (cnt2 - 1);
+					cnt2=0;
+				}
+				else{
+					cnt2++;
+					if (cnt > 0)A += (cnt - 1);
+					cnt = 0;
+				}
+			}
+			if( cnt>0) A+= cnt-1;
+			if( cnt2>0) A+= cnt2-1;
+			cout << A << endl;
+		}
+	}
+
+
+}
