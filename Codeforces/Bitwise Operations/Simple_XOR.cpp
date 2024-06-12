36792e72d8bc09e93aad365af406a501603f19a5
Pratham Pithalia <144723251+prathampithalia@users.noreply.github.com>
2024-06-12 17:13:49 +0530

    S

---
+++ Codeforces/Bitwise Operations/Simple_XOR.cpp
@@ -0,0 +1,54 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define all(a) a.begin(), a.end()
+#define fi first
+#define se second
+#define pb push_back
+#define pyes cout << "YES" << endl
+#define pno cout << "NO" << endl
+#define ce cout << endl
+#define forr(i, n) for (int i = 0; i < n; i++)
+#define each(a, x) for (auto & a: x)
+#define pvec(x) for (auto & a: x) { cout << a << " ";}
+#define pvec2d(v) for (int i = 0; i < v.size(); i++) {for (int j = 0; j < v[i].size(); j++) {cout << v[i][j] << " ";}cout << endl;}
+#define ppair(x) each(i, x) cout << i.first << " " << i.second << endl;
+
+#define ll long long
+#define MOD 1000000007
+#define test_cases(T) int T; cin >> T; while (T--)
+#define vec(v,n,int) vector<int> v(n) ; forr(i,n)cin>>v[i];
+#define vec2d(v,n,m,int) vector<vector<int>> v(n,vector<int> (m,0)); forr(i, n)forr(j, m) cin >> v[i][j];
+
+
+
+
+signed main() {
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+test_cases(Y){
+    int l , r; cin>>l>>r;
+
+    if( (l&1)== 0){
+        cout << l<<' '<<l+1<<' '<<l+2<<' '<<l+3;
+    }
+    else{
+        l++;
+        if( l+3 > r) cout << -1 ;
+
+        else cout << l<<' '<<l+1<<' '<<l+2<<' '<<l+3;
+
+    }
+    ce;
+}
+
+
+}
