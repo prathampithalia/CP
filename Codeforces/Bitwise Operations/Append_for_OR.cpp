f00784608af0aad8bc53c0ecae34ff8060bc7e39
Pratham Pithalia <144723251+prathampithalia@users.noreply.github.com>
2024-06-12 15:36:47 +0530

    S

---
+++ Codeforces/Bitwise Operations/Append_for_OR.cpp
@@ -0,0 +1,76 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define all(a) a.begin(), a.end()
+#define fi first
+#define se second
+#define pb push_back
+#define pyes cout << "YES" << endl
+#define pno cout << "NO" << endl
+#define ce cout << endl
+#define forr(i, n) for (int i = 0; i < n; i++)
+#define each(a, x) for (auto & a: x)
+#define pvec(x) for (auto & a: x) { cout << a << " ";}
+#define pvec2d(v) for (int i = 0; i < v.size(); i++) {for (int j = 0; j < v[i].size(); j++) {cout << v[i][j] << " ";}cout << endl;}
+#define ppair(x) each(i, x) cout << i.first << " " << i.second << endl;
+
+#define ll long long
+#define MOD 1000000007
+#define test_cases(T) int T; cin >> T; while (T--)
+#define vec(v,n,int) vector<int> v(n) ; forr(i,n)cin>>v[i];
+#define vec2d(v,n,m,int) vector<vector<int>> v(n,vector<int> (m,'0')); forr(i, n)forr(j, m) cin >> v[i][j];
+
+
+
+#ifndef ONLINE_JUDGE
+#include "template.cpp"
+#else
+#define debug(...)
+#define debugArr(...):
+#endif
+
+
+
+
+
+signed main() {
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    test_cases(Y) {
+        int n, y; cin >> n >> y;
+        vector<int> v(n);
+        int A = 0;
+        forr(i, n) {
+            cin >> v[i];
+            A |= v[i];
+        }
+
+        int ans = 0;
+        bool notposs = false;
+        for (int i = 0; i < 31;i++) {
+
+            if ((bool)(A & (1 << i)) == 1 && (bool)(y & (1 << i)) == 0)
+            {
+                notposs = true;
+            }
+            if ((bool)(A & (1 << i)) == 0 && (bool)(y & (1 << i)) == 1) {
+                ans += (1 << i);
+            }
+        }
+        if (notposs)cout << -1 << endl;
+        else cout << ans << endl;
+
+
+    }
+
+
+}
+
