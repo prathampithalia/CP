1cfdfd9c6b8b98a589d31d6ccfe27217e2593779
Pratham Pithalia <144723251+prathampithalia@users.noreply.github.com>
2024-06-29 02:14:58 +0530

    S : maths gcd

---
+++ A_Exciting_Bets.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+//#include<vector>
+
+using namespace std;
+#define int long long
+
+void solve() {
+    int a, b;
+    cin >> a >> b;
+
+    if (a == b) cout << 0 << ' ' << 0 << endl;
+    else {
+
+        int d = abs(a - b);
+
+        cout << d << ' ' << min(d - (a % d), a % d) << endl;
+
+    }
+
+}
+
+signed main() {
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    ios_base::sync_with_stdio(0); cin.tie(0);
+
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+}
