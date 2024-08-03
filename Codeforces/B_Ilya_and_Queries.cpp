baa6929d87754e1e1577196cd4711daa22f3cf3f
prathampithalia <prathampithalia100@gmail.com>
2024-08-03 16:07:13 +0530

    S

--- Codeforces/B_Ilya_and_Queries.cpp
+++ Codeforces/B_Ilya_and_Queries.cpp
@@ -39,17 +39,17 @@ using namespace std;
 
 void solve() {
 
-    string s; cin >> s;
-    int n = s.size();
+    string s ; cin >> s;
+    int n = s.size() ;
     int q; cin >> q;
-    vi dp(n + 1);
-    for (int i = 0; i < n - 1; i++) {
-        dp[i + 1] = dp[i] + (s[i] == s[i + 1]);
+    vi dp(n+1);
+    for (int i = 0 ; i < n - 1 ; i++) {
+        dp[i+1] = dp[i] + (s[i] == s[i + 1]);
     }
 
-    while (q--) {
-        int l, r; cin >> l >> r;
-        l--; r--;
+    while (q--) {   
+        int l , r; cin >> l >> r ;
+        l-- ; r-- ;
         cout << dp[r] - dp[l] << endl;
     }
 
