f409329b58fc3cf0427d8763ae889c24d3095970
prathampithalia <prathampithalia100@gmail.com>
2024-08-19 02:39:39 +0530

    S : Strings

---
+++ Codeforces/D_Backspace.cpp
@@ -0,0 +1,82 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define all(a)   a.begin(), a.end()
+#define fi       first
+#define se       second
+#define pb       push_back
+#define pyes     cout << "YES" << endl
+#define pno      cout << "NO" << endl
+#define ce       cout << endl
+#define forr(i, n) for (int i = 0; i < n; i++)
+#define each(a, x) for (auto & a: x)
+#define pvec(x)    for (auto & a: x) { cout << a << " ";}
+#define pvec2d(v)  for (int i = 0; i < v.size(); i++) {for (int j = 0; j < v[i].size(); j++) {cout << v[i][j] << " ";}cout << endl;}
+#define ppair(x)   each(i, x) cout << i.first << " " << i.second << endl;
+
+#define ll    long long
+#define MOD   1000000007
+#define vec(v,n,int)     vector<int> v(n) ; forr(i,n)cin>>v[i];
+#define vec2d(v,n,m,int) vector<vector<int>> v(n,vector<int> (m,0)); forr(i, n)forr(j, m) cin >> v[i][j];
+#define test_cases(T) int T; cin >> T; while (T--)
+
+#define pii   pair<int,int>
+#define vi    vector<int>
+#define vpi   vector<pair<int,int>>
+#define umap  unordered_map
+#define maxpq(int) priority_queue<int>
+#define minpq(int) priority_queue<int, vector<int>, greater<int> >
+
+
+
+#define MIN(v)    *min_element(all(v))
+#define MAX(v)    *max_element(all(v))
+#define LB(c, x)  distance((c).begin(), lower_bound(all(c), (x)))
+#define UB(c, x)  distance((c).begin(), upper_bound(all(c), (x)))
+#define UNIQUE(x) sort(all(x)), x.erase(unique(all(x)), x.end()), x.shrink_to_fit()
+
+
+void solve() {
+    unordered_map<string, int> mp;
+    string s, t; cin >> s >> t;
+
+    int n = s.size(), m = t.size();
+
+    if (n < m) {
+        pno;
+        return;
+    }
+
+    int j = m - 1, i = n - 1;
+    while (i >= 0) {
+        if (s[i] != t[j]) {
+            i -= 2;
+        }
+        else
+        {
+            j--; i--;
+        }
+        if (j == -1) {
+            pyes;
+            return;
+        }
+
+    }
+    pno;
+}
+
+signed main() {
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    test_cases(U)solve();
+
+
+}
