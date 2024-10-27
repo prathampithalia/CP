0e4799d859ec50927e3b263cf7219952d05cda51
prathampithalia <prathampithalia100@gmail.com>
2024-10-28 00:01:49 +0530

    S

---
+++ CP-31 Sheet/900/B_Bad_Boy.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+#define int long long
+
+void solve() {
+    int n, m, i, j;
+    cin >> n >> m >> i >> j;
+
+    cout << n << ' ' << m << ' ' << 1 << ' ' << 1 << endl;
+
+}
+
+signed main() {
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    ios_base::sync_with_stdio(0); cin.tie(0);
+
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+}
