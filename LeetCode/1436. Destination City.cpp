e4427e88950629ee613af317b7519e98ea910498
prathampithalia <prathampithalia100@gmail.com>
2024-10-31 03:36:54 +0530

    S

---
+++ LeetCode/1436. Destination City.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    string destCity(vector<vector<string>>& paths) {
+        string ans;
+        map<string, string> mp;
+        for (auto i : paths) {
+            mp[i[0]] = i[1];
+
+        }
+        while (ans) {
+
+        }
+    }
+};
