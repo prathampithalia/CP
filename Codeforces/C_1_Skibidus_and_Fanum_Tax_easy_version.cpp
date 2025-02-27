4b5771c479563edf5d3791daaf920c33c5fc00d8
prathampithalia <prathampithalia100@gmail.com>
2025-02-27 13:14:17 +0530

    S

---
+++ Codeforces/C_1_Skibidus_and_Fanum_Tax_easy_version.cpp
@@ -0,0 +1,88 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define all(a)   a.begin(), a.end()
+#define fi       first
+#define se       second
+#define pb       push_back
+#define pyes     cout << "YES" << endl
+#define pno      cout << "NO" << endl
+#define ce       cout << endl
+#define forr(i, n) for (int i = 0; i < n; i++)
+#define each(a, x) for (auto & a: x)
+#define pvec(x)    for (auto & a: x) { cout << a << " ";}
+#define pvec2d(v)  for (int i = 0; i < v.size(); i++) {for (int j = 0; j < v[i].size(); j++) {cout << v[i][j] << " ";}cout << endl;}
+#define ppair(x)   each(i, x) cout << i.first << " " << i.second << endl;
+
+#define ll    long long
+#define MOD   1000000007
+#define vec(v,n,int)     vector<int> v(n) ; forr(i,n)cin>>v[i];
+#define vec2d(v,n,m,int) vector<vector<int>> v(n,vector<int> (m,0)); forr(i, n)forr(j, m) cin >> v[i][j];
+#define test_cases(T) int T; cin >> T; while (T--)
+
+#define pii   pair<int,int>
+#define vi    vector<int>
+#define vpi   vector<pair<int,int>>
+#define umap  unordered_map
+#define maxpq(int) priority_queue<int>
+#define minpq(int) priority_queue<int, vector<int>, greater<int> >
+
+
+
+#define MIN(v)    *min_element(all(v))
+#define MAX(v)    *max_element(all(v))
+#define LB(c, x)  distance((c).begin(), lower_bound(all(c), (x)))
+#define UB(c, x)  distance((c).begin(), upper_bound(all(c), (x)))
+#define UNIQUE(x) sort(all(x)), x.erase(unique(all(x)), x.end()), x.shrink_to_fit()
+
+
+#ifndef ONLINE_JUDGE
+#include "template.cpp"
+#else
+#define debug(...)
+#define debugArr(...):
+#endif
+
+
+
+void solve() {
+    int n, m; cin >> n >> m;
+
+    vec(a, n, int);
+    vec(b, m, int);
+
+
+    for (int i = 0; i < n; i++)
+    {
+        a[i] = min(a[i], b[0] - a[i]);
+        if (i)
+        {
+            if (a[i] < a[i - 1]) a[i] = max(a[i], b[0] - a[i]);
+        }
+    }
+    for (int i = 0; i < n - 1; i++)
+    {
+        if (a[i] > a[i + 1]) {
+            pno;
+            return;
+        }
+    }
+    pyes;
+
+}
+
+
+signed main() {
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    test_cases(U)solve();
+
+}
