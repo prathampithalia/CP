3fdeb688b2e4599211b5c673e159634df7132f23
prathampithalia <prathampithalia100@gmail.com>
2025-03-26 00:37:39 +0530

    S : solution Optimized

--- LeetCode/142. Linked List Cycle II.cpp
+++ LeetCode/142. Linked List Cycle II.cpp
@@ -10,18 +10,38 @@ class Solution {
 public:
     ListNode* detectCycle(ListNode* head) {
 
-        if (head == NULL || head->next == NULL) return NULL;
+        if (head == NULL || head->next == NULL)
+            return NULL;
 
-        unordered_map<ListNode*, int > mp;
+        // unordered_map<ListNode* , int > mp ;
 
-        while (head->next != NULL) {
-            if (mp[head]) return head;
-            mp[head]++;
-            head = head->next;
+        // while( head->next != NULL ){
+        //     if( mp[head] ) return head ;
+        //     mp[head]++ ;
+        //     head = head -> next ;
 
+        // }
+
+        // return NULL ;
+
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while (fast != NULL && fast->next != NULL) {
+
+            slow = slow->next;
+            fast = fast->next->next;
+
+            if (slow == fast) {
+                slow = head;
+                while (slow != fast) {
+                    slow = slow->next;
+                    fast = fast->next;
+                }
+                return slow;
+            }
         }
 
         return NULL;
-
     }
 };
