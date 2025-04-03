5ed764de187b664bff31388d26905f7ca8ad8e01
prathampithalia <prathampithalia100@gmail.com>
2025-04-04 00:21:08 +0530

    S

---
+++ LeetCode/404. Sum of Left Leaves.cpp
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+void solve(TreeNode* root, int& ans) {
+    if (root == NULL)return;
+
+    if (root->left != NULL && root->left->left == NULL && root->left->right == NULL) {
+        ans += root->left->val;
+    }
+    solve(root->left, ans);
+    solve(root->right, ans);
+
+
+}
+
+class Solution {
+public:
+    int sumOfLeftLeaves(TreeNode* root) {
+        int ans = 0;
+        solve(root, ans);
+
+        return ans;
+    }
+};
