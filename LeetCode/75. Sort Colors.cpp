81d5c677fc8210b227646414d90a263f9995713f
prathampithalia <prathampithalia100@gmail.com>
2025-05-13 00:29:47 +0530

    S

---
+++ LeetCode/75. Sort Colors.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        map<int, int> mp;
+        for (auto i : nums)mp[i]++;
+        for (int i = 0; i < nums.size(); i++) {
+            if (mp[0] > 0) {
+                nums[i] = 0;
+                mp[0]--;
+            }
+            else if (mp[1] > 0) {
+                nums[i] = 1;
+                mp[1]--;
+            }
+            else {
+                nums[i] = 2;
+                mp[2]--;
+            }
+        }
+    }
+};
