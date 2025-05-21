1b71d1381823ec57a9ed819a7cdfa6116aff9e6f
prathampithalia <prathampithalia100@gmail.com>
2025-05-22 00:20:28 +0530

    S : BS , exactly as Split Array Problem

---
+++ LeetCode/The Painter's Partition Problem-II.cpp
@@ -0,0 +1,44 @@
+// User function template for C++
+
+class Solution {
+public:
+    int helper(vector<int> a, int sum) {
+
+        int partitions = 1;
+        int tot = 0;
+        for (int i = 0; i < a.size(); i++) {
+            if (a[i] + tot <= sum) {
+                tot += a[i];
+            }
+            else {
+                partitions++;
+                tot = a[i];
+            }
+        }
+        return partitions;
+    }
+
+    int minTime(vector<int>& nums, int k) {
+        // code here
+        int low = *max_element(nums.begin(), nums.end());
+        int high = accumulate(nums.begin(), nums.end(), 0);
+
+        int mid = low + (high - low) / 2;
+
+        while (low <= high) {
+
+            if (helper(nums, mid) > k) {
+                low = mid + 1;
+            }
+            else {
+                high = mid - 1;
+            }
+
+            mid = low + (high - low) / 2;
+        }
+
+
+        return low;
+        // return minimum time
+    }
+};
