583495fc6d72def23adafb4296f0c02ac60653ad
prathampithalia <prathampithalia100@gmail.com>
2025-05-22 23:36:05 +0530

    S : BS sol

--- LeetCode/1539. Kth Missing Positive Number.cpp
+++ LeetCode/1539. Kth Missing Positive Number.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     int findKthPositive(vector<int>& arr, int k) {
-       
-       int n = arr.size();
+
+        int n = arr.size();
         int cnt = 0, j = 0, mis = 0;
         for (int i = 1; i <= 20000; i++) {
-            if ( j<n && i == arr[j] ) {
+            if (j < n && i == arr[j]) {
                 j++;
-            } 
-            else  {
+            }
+            else {
                 mis = i;
                 cnt++;
             }
@@ -17,5 +17,20 @@ public:
             }
         }
         return 0;
+    }
+};
+
+class Solution {
+public:
+    int findKthPositive(vector<int>& arr, int k) {
+        int n = arr.size();
+        vector<int> a(n);
+
+        for (int i = 0;i < n;i++)a[i] = arr[i] - (i + 1);
+
+        auto ind = lower_bound(a.begin(), a.end(), k);
+
+        return k + ind - a.begin();
+
     }
 };
