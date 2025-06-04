8fce22894ceacfb9388d0d59184e5a8f276b6784
prathampithalia <prathampithalia100@gmail.com>
2025-06-04 16:29:05 +0530

    S : 2 approach updated

--- LeetCode/199. Binary Tree Right Side View.cpp
+++ LeetCode/199. Binary Tree Right Side View.cpp
@@ -12,28 +12,50 @@
  */
 class Solution {
 public:
+    void rightCall(TreeNode* root, int lvl, vector<int>& ans) {
+        if (root == NULL)
+            return;
+        if (ans.size() == lvl) {
+            ans.push_back(root->val);
+        }
+        rightCall(root->right, lvl + 1, ans);
+        rightCall(root->left, lvl + 1, ans);
+    }
+    void leftCall(TreeNode* root, int lvl, vector<int>& ans) {
+        if (root == NULL)
+            return;
+        if (ans.size() == lvl) {
+            ans.push_back(root->val);
+        }
+        leftCall(root->left, lvl + 1, ans);
+        leftCall(root->right, lvl + 1, ans);
+    }
     vector<int> rightSideView(TreeNode* root) {
 
-        if (root == NULL) return {};
-        map<int, vector<int>> mp;
-        queue<pair<TreeNode*, int>> q;
+        if (root == NULL)
+            return {};
+        // map<int, vector<int>> mp;
+        // queue<pair<TreeNode*, int>> q;
 
-        q.push({ root, 0 });
-        while (!q.empty()) {
-            auto it = q.front().first;
-            auto lvl = q.front().second;
+        // q.push({root, 0});
+        // while (!q.empty()) {
+        //     auto it = q.front().first;
+        //     auto lvl = q.front().second;
 
-            mp[lvl].push_back(it->val);
+        //     mp[lvl].push_back(it->val);
 
-            q.pop();
-            if (it->left)
-                q.push({ it->left, lvl + 1 });
-            if (it->right)
-                q.push({ it->right, lvl + 1 });
-        }
+        //     q.pop();
+        //     if (it->left)
+        //         q.push({it->left, lvl + 1});
+        //     if (it->right)
+        //         q.push({it->right, lvl + 1});
+        // }
+        // vector<int> ans;
+        // for (auto i : mp)
+        //     ans.push_back(i.second.back());
         vector<int> ans;
-        for (auto i : mp)
-            ans.push_back(i.second.back());
+
+        rightCall(root, 0, ans);
         return ans;
     }
 };
