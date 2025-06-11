1cb64d64d22a3582a0f8f60501169eac035118f3
prathampithalia <prathampithalia100@gmail.com>
2025-06-11 15:58:21 +0530

    S : DP

--- LeetCode/213. House Robber II.cpp
+++ LeetCode/213. House Robber II.cpp
@@ -1,36 +1,50 @@
-int helper(vector<int> a) {
-
-    int n = a.size();
-    vector<int> dp(n + 1, -1);
-    if (n == 1)
-        return a[0];
-
-    dp[0] = a[0];
-    dp[1] = max(a[0], a[1]);
-    for (int i = 2; i < n; i++) {
-        dp[i] = max(dp[i - 1], dp[i - 2] + a[i]);
-    }
-
-    return dp[n - 1];
-}
 
 class Solution {
 public:
-    int rob(vector<int>& nums) {
+    int tabulation(vector<int>& nums) {
 
+        int n = nums.size();
+
+        vector<int> dp(n + 1);
+
+        dp[0] = nums[0];
+        if (n == 1)
+            return dp[0];
+        dp[1] = max(nums[0], nums[1]);
+
+        for (int i = 2; i < n; i++) {
+            dp[i] = max(dp[i - 1], dp[i - 2] + nums[i]);
+        }
+        return dp[n - 1];
+    }
+
+    int spaceOptimized(vector<int> nums) {
         int n = nums.size();
         if (n == 1)
             return nums[0];
-        vector<int> a1, a2;
-        for (int i = 0; i < n; i++) {
-            if (i != 0)
-                a1.push_back(nums[i]);
-            if (i != n - 1)
-                a2.push_back(nums[i]);
+        int prev2 = nums[0];
+        int prev = max(nums[0], nums[1]);
+        int cur = 0;
+
+        for (int i = 2; i < n; i++) {
+            cur = max(prev2 + nums[i], prev);
+            prev2 = prev;
+            prev = cur;
         }
 
-        int ans = max(helper(a1), helper(a2));
+        return prev;
+    }
 
-        return ans;
+    int rob(vector<int>& nums) {
+
+        vector<int> a1, a2;
+        if (nums.size() == 1) return nums[0];
+        for (int i = 0; i < nums.size(); i++) {
+            if (i != 0)
+                a1.push_back(nums[i]);
+            if (i != nums.size() - 1)
+                a2.push_back(nums[i]);
+        }
+        return max(spaceOptimized(a1), spaceOptimized(a2));
     }
 };
