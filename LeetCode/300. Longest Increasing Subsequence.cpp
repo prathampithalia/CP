9034fd91bfbf8d126c19c5b6c8de93b944a5a440
prathampithalia <prathampithalia100@gmail.com>
2025-06-15 20:42:25 +0530

    S : DP

--- LeetCode/300. Longest Increasing Subsequence.cpp
+++ LeetCode/300. Longest Increasing Subsequence.cpp
@@ -1,16 +1,51 @@
 class Solution {
 public:
-    int lengthOfLIS(vector<int>& nums) {
-        int n = nums.size();
-        int ans = 1;
-        vector<int> dp(n);
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < i; j++) {
-                if (nums[i] > nums[j] && dp[i] < dp[j] + 1) {
-                    dp[i] = dp[j] + 1;
-                }
+    int lengthOfLIS(vector<int>& arr) {
+        // int n = nums.size();
+        // int ans = 1;
+        // vector<int> dp(n);
+        // for (int i = 0; i < n; i++) {
+        //     for (int j = 0; j < i; j++) {
+        //         if (nums[i] > nums[j] && dp[i] < dp[j] + 1) {
+        //             dp[i] = dp[j] + 1;
+        //         }
+        //     }
+        // }
+        // return *max_element(dp.begin(), dp.end()) + 1;
+        // int n = s1.size();
+        // vector<vector<int>>dp(n + 1, vector<int>(n + 1, 0)) ;
+
+        // int ans = 1;
+        // for (int i = 1; i <= n; i++) {
+        //     for (int j = 1; j <= i; j++) {
+        //         if (s1[i - 1] < s1[j - 1]) {
+        //             dp[i][j] = 1+dp[i-1][j-1];
+        //         } else
+        //             dp[i][j] = max(dp[i - 1][j] , dp[i][j-1]);
+        //         ans = max(ans, dp[i][j]);
+        //     }
+        // }
+        // return ans ;
+
+        int n = arr.size();
+        vector<int> temp ;
+        
+        temp.push_back(arr[0]);
+
+        for(int i = 1 ; i<n ; i++){
+            if( arr[i] > temp.back()){
+                temp.push_back(arr[i]);
+            }
+            else{
+                int ind = lower_bound(temp.begin(),temp.end() , arr[i]) - temp.begin();
+                temp[ind] = arr[i];
             }
         }
-        return *max_element(dp.begin(), dp.end()) + 1;
+
+        return temp.size();
+
     }
 };
+
+
+    
