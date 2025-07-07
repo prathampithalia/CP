797f4dc599a859768781db01673a362c4aedcb84
prathampithalia <prathampithalia100@gmail.com>
2025-07-08 02:29:51 +0530

    S : optimized sol added

--- LeetCode/739_daily_temperatures.cpp
+++ LeetCode/739_daily_temperatures.cpp
@@ -1,45 +1,53 @@
 class Solution {
 public:
-    vector<int> dailyTemperatures(vector<int>& temperatures) {
 
-int n= temperatures.size();
+    /*
 
-    vector<int> v; 
-    stack<pair<int,int>> s;
-    //  Nearest Greatest to Right Variation
+        NGR method :
+                stack<pair<int, int>> s;
+            int n = temp.size();
 
-    for (int i = n - 1;i >= 0;i--) {
-        if (s.size() == 0){
-            v.push_back(0);
-        }
-        else if (s.size() > 0 && s.top().first > temperatures[i])
-        {
-            v.push_back(s.top().second);
-        }
-        else if (s.size() > 0 && s.top().first <= temperatures[i])
-        {
-            while (s.size() > 0 && s.top().first <= temperatures[i])
-            {
+            vector<int> v;
+            for (int i = n - 1; i >= 0; i--) {
+                if (s.size() == 0) {
+                    v.push_back(0);
+                } else {
+                    if (s.top().first > temp[i]) {
+                        v.push_back(s.top().second);
+                    } else {
+                        while (s.size() && s.top().first <= temp[i])
+                            s.pop();
+                        if (s.size() == 0) {
+                            v.push_back(0);
+                        } else {
+                            v.push_back(s.top().second);
+                        }
+                    }
+                }
+                s.push({temp[i], i});
+            }
+
+            reverse(v.begin(), v.end());
+            for (int i = 0; i < n; i++) {
+                if (v[i] != 0)
+                    v[i] = v[i] - i;
+            }
+            return v;
+    */
+    vector<int> dailyTemperatures(vector<int>& temp) {
+
+        int n = temp.size();
+        vector<int> res(n);
+        stack<int> s;
+        for (int i = 0; i < n; i++) {
+            while (!s.empty() && temp[s.top()] < temp[i]) {
+                res[s.top()] = i - s.top();
                 s.pop();
             }
-            if (s.size() == 0)
-            {
-                v.push_back(0);
-            }
-            else
-            {
-                v.push_back(s.top().second);
-            }
-        }
-        s.push({temperatures[i] , i});
-    }
-    reverse(v.begin(),v.end()); 
-    
-for(int i =0 ; i<n ;i++){
-    if(v[i] != 0) v[i] = v[i] - i;
-}
 
-return v;
+            s.push(i);
+        }
+        return res;
 
     }
 };
