f207c62d03ebb4b39e6853d479b7edb5a044dd65
prathampithalia <prathampithalia100@gmail.com>
2025-07-13 15:21:27 +0530

    S : optimized

--- LeetCode/2410. Maximum Matching of Players With Trainers.cpp
+++ LeetCode/2410. Maximum Matching of Players With Trainers.cpp
@@ -2,21 +2,38 @@ class Solution {
 public:
     int matchPlayersAndTrainers(vector<int>& players, vector<int>& trainers) {
 
-        multiset<int> ms;
-        for (auto i : trainers) {
-            ms.insert(i);
-        }
+        // multiset<int> ms;
+        // for (auto i : trainers) {
+        //     ms.insert(i);
+        // }
 
+        // sort(players.begin(), players.end());
+
+        // int count = 0;
+        // for (int i = 0; i < players.size(); i++) {
+
+        //     auto it = ms.lower_bound(players[i]);
+        //     if (it != ms.end()) {
+        //         count++;
+        //         ms.erase(ms.find(*it));
+        //     }
+
+        // }
         sort(players.begin(), players.end());
+        sort(trainers.begin(), trainers.end());
 
         int count = 0;
-        for (int i = 0; i < players.size(); i++) {
 
-            auto it = ms.lower_bound(players[i]);
-            if (it != ms.end()) {
+        int i = 0, j = 0;
+        int n = players.size(), m = trainers.size();
+
+        while (i < n && j < m) {
+
+            if (players[i] <= trainers[j]) {
                 count++;
-                ms.erase(ms.find(*it));
+                i++;
             }
+            j++;
 
         }
         return count;
