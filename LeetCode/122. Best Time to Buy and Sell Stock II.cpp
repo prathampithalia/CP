d646e95abdec0ecec7614d8c6ed875b1237a1765
prathampithalia <prathampithalia100@gmail.com>
2025-08-05 21:53:23 +0530

    S :memo , tabulation,  best

--- LeetCode/122. Best Time to Buy and Sell Stock II.cpp
+++ LeetCode/122. Best Time to Buy and Sell Stock II.cpp
@@ -29,20 +29,19 @@ public:
     }
     int tabulation(vector<int>& prices) {
         int n = prices.size();
-        vector<vector<int>> dp(n + 1, vector<int>(2, 0));
+        if (n == 0)
+            return 0;
+        vector<array<int, 2>> dp(n + 1);
+        const int NEG = -1e9;
+        dp[0][0] = 0;
+        dp[0][1] = NEG;
 
-        for (int i = 1; i <= n; i++) {
-            for (int buy = 0; buy <= 1; buy++) {
-                if (buy) {
-                    dp[i][buy] = max(dp[i - 1][buy], -prices[i - 1] + dp[i - 1][0]);
-                }
-                else {
-                    dp[i][buy] = max(dp[i - 1][buy], prices[i - 1] + dp[i - 1][1]);
-                }
-            }
+        for (int i = 1; i <= n; ++i) {
+            int p = prices[i - 1];
+            dp[i][1] = max(dp[i - 1][1], dp[i - 1][0] - p);
+            dp[i][0] = max(dp[i - 1][0], dp[i - 1][1] + p);
         }
-
-        return dp[n][1];
+        return dp[n][0];
     }
 
     int maxProfit(vector<int>& prices) {
